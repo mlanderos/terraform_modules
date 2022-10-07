ba93b0dfd1373b9011048d61de4b0f76d98e1891
Michael <michael.landeros@gmail.com>
2022-10-07 15:10:51 -0500

    Create outputs.tf

---
+++ modules/resources/networking/vnet/outputs.tf
@@ -0,0 +1,19 @@
+output "virtualNetworkName" {
+  value = azurerm_virtual_network.virtual_network.name
+}
+
+output "virtualNetworkLocation" {
+  value = azurerm_virtual_network.virtual_network.location
+}
+
+output "virtualNetworkResourceGroupName" {
+  value = azurerm_virtual_network.virtual_network.resource_group_name
+}
+
+output "virtualNetworkAddressSpace" {
+  value = azurerm_virtual_network.virtual_network.address_space
+}
+
+output "virtualNetworkGUID" {
+  value = azurerm_virtual_network.virtual_network.guid
+}
